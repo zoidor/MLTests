e4337826fdfe129a63b97c537f8e0f7bc02b8be3
msalmi86 <msalmi86@gmail.com>
2018-12-18 20:05:17 +0100

    now the test runs simply with bazel run :cell_segmentation

--- segmentation/deployment/main.cc
+++ segmentation/deployment/main.cc
@@ -1,11 +1,15 @@
 #include <fstream>
 #include <utility>
+#include <string>
 #include <vector>
+
+#include <cstdlib>
 
 #include "tensorflow/cc/ops/standard_ops.h"
 #include "tensorflow/core/platform/init_main.h"
 #include "tensorflow/core/public/session.h"
 #include "tensorflow/core/framework/tensor_shape.h"
+#include "tensorflow/core/lib/io/path.h"
 
 namespace { //anonymous namespace
 
@@ -32,6 +36,7 @@ std::unique_ptr<tensorflow::Session> LoadGraph(const std::string& graph_file_nam
     tensorflow::Status load_graph_status =
       ReadBinaryProto(tensorflow::Env::Default(), graph_file_name, &graph_def);
   if (!load_graph_status.ok()) {
+        std::cout << "Load failed, error: " << load_graph_status << '\n';
 	return nullptr;
   }
 
@@ -45,12 +50,12 @@ std::unique_ptr<tensorflow::Session> LoadGraph(const std::string& graph_file_nam
 
   return session;
 }
+
 }//END anonymous namespace
 
 int main(int argc, char* argv[]) {
 
-  const char* image_path = "input.jpeg";
-  const char* model_path = "test_model_tensorflow.pb";
+  const char* model_path = "segmentation/data/test_model_tensorflow.pb";
  
   const int cropped_input_height = 512;
   const int cropped_input_width = 688;
@@ -62,7 +67,7 @@ int main(int argc, char* argv[]) {
 
   // We need to call this to set up global state for TensorFlow.
   tensorflow::port::InitMain(argv[0], &argc, &argv);
-  
+
   // First we load and initialize the model.
   auto session = LoadGraph(model_path);
   if (!session) {
@@ -74,7 +79,8 @@ int main(int argc, char* argv[]) {
   // to the specifications the main graph expects.
   tensorflow::Tensor resized_tensor;
   tensorflow::Status read_tensor_status =
-          ReadTensorFromTiff(image_path, cropped_input_height , cropped_input_width, resized_tensor);
+          ReadTensorFromTiff("", cropped_input_height , cropped_input_width, resized_tensor);
+
 
   if (!read_tensor_status.ok()) {
     LOG(ERROR) << read_tensor_status;
