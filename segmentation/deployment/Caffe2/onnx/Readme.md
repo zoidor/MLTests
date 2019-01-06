8b9a7ed25b5168170ef5dcc9dba4b4974991f99b
msalmi86 <msalmi86@gmail.com>
2019-01-06 14:26:15 +0100

    trying to pass through onnx, still issues

--- segmentation/deployment/Caffe2/onnx/Readme.md
+++ segmentation/deployment/Caffe2/onnx/Readme.md
@@ -1,5 +1,11 @@
 pip(3) install git+https://github.com/onnx/onnxmltools
-pip(3) install onnx_caffe2
-pip(3) install future
+install pytorch 
+#pip(3) install future <-- If you have an error as "past"
 
 python(3) script.py
+
+
+pip install git+https://github.com/onnx/onnxmltools
+
+pip install git+https://github.com/onnx/onnx
+
--- segmentation/deployment/Caffe2/onnx/script.py
+++ segmentation/deployment/Caffe2/onnx/script.py
@@ -1,8 +1,9 @@
 import onnxmltools
 import keras.models
 
-import onnx
-from onnx_caffe2.backend import Caffe2Backend
+import caffe2.python.onnx
+#from torch.onnx_caffe2.backend import Caffe2Backend
+from caffe2.python.onnx.backend import Caffe2Backend
 
 keras_model = keras.models.load_model("../../../learning/test_model.h5")
 onnx_model = onnxmltools.convert_keras(keras_model) 
@@ -15,3 +16,7 @@ with open("init_net.pb", "wb") as f:
 with open("predict_net.pb", "wb") as f:
     f.write(predict_net.SerializeToString())
 
+
+
+with open("onnx-predict.pbtxt", "w") as f:
+    f.write(str(predict_net))
