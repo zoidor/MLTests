cf0f1b79df8d681914db1e9d52cf76d7ef3a89bd
msalmi86 <msalmi86@gmail.com>
2019-02-06 22:22:05 +0100

    Removed spatial in BatchNormalization as it was done in https://github.com/pytorch/pytorch/pull/9492

--- malaria/Caffe2/modelTranslation/convertFromKerasToCaffe2.py
+++ malaria/Caffe2/modelTranslation/convertFromKerasToCaffe2.py
@@ -1,9 +1,19 @@
 import onnxmltools
 import keras.models
 import os
+import sys
 
 import caffe2.python.onnx
 from caffe2.python.onnx.backend import Caffe2Backend
+
+import google.protobuf.message
+
+def stripField(proto):
+	for node in proto.graph.node:
+		if(node.op_type == "BatchNormalization"):
+			newL = [a for a in node.attribute if not (a.name == "spatial" and a.i == 1) ]
+			del node.attribute[:]
+			node.attribute.extend(newL)		
 
 def safe_mkdir(dirpath):
 	if not os.path.exists(dirpath):
@@ -16,6 +26,7 @@ safe_mkdir(output_dir)
 keras_model = keras.models.load_model("../../kerasModel/malaria_model.h5")
 onnx_model = onnxmltools.convert_keras(keras_model) 
 onnxmltools.utils.save_model(onnx_model, output_dir + "test_model.onnx")
+stripField(onnx_model)
 
 init_net, predict_net = Caffe2Backend.onnx_graph_to_caffe2_net(onnx_model)
 
